f90b898817f06e8b8ce50293db78d2560810e4ef
DireanX <143181108+DireanX@users.noreply.github.com>
2023-08-25 14:01:55 +0300

    айта-визитки

---
+++ js.js
@@ -0,0 +1,6 @@
+const likeButton = document.querySelector('.like-button');
+const likeCount = document.querySelector('.like-count');
+ 
+likeButton.addEventListener('click', function() {
+  alert('Спасибо')
+})
